e1fb3a612c8333871c753fff3a16879452b82bd8
Santiago Mendez <santi17mendez@hotmail.com>
2019-10-27 17:46:00 -0300

    Angular CLASE 11

--- Codigo/Angular/src/app/app.module.ts
+++ Codigo/Angular/src/app/app.module.ts
@@ -1,24 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeworksListComponent } from './homeworks-list/homeworks-list.component';
 import { HomeworksFilterPipe } from './homeworks-list/homeworks-filter.pipe';
 import { HomeworksService } from './services/homeworks.service';
+import { StarComponent } from './star/star.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HttpModule } from '@angular/http';
+import { HomeworkDetailComponent } from './homework-detail/homework-detail.component';
+import { HomeworkDetailGuard } from './guards/homework-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeworksListComponent,
-    HomeworksFilterPipe
+    HomeworksFilterPipe,
+    StarComponent,
+    WelcomeComponent,
+    HomeworkDetailComponent
   ],
   imports: [
+    HttpModule,
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: 'homeworks', component: HomeworksListComponent },
+        { path: 'homeworks/:id', 
+          component: HomeworkDetailComponent,
+          canActivate: [HomeworkDetailGuard]
+        },
+        { path: 'welcome', component:  WelcomeComponent }, 
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+        { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [
-    HomeworksService
+    HomeworksService,
+    HomeworkDetailGuard
   ],
   bootstrap: [AppComponent]
 })
--- Codigo/Angular/src/app/services/homeworks.service.ts
+++ Codigo/Angular/src/app/services/homeworks.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Http, Response, RequestOptions, Headers } from "@angular/http";
+import { Observable, throwError } from "rxjs"; 
+import { map, tap, catchError } from 'rxjs/operators';
 import { Homework } from '../models/Homework';
-import { Exercise } from '../models/Exercise';
 
 @Injectable()
 export class HomeworksService {
 
-  constructor() { }
+  private WEB_API_URL : string = 'http://localhost:5000/api/homeworks';
 
-  getHomeworks():Array<Homework> {
-    return [
-      new Homework('1', 'Una tarea', 0, new Date(), [
-        new Exercise('1', 'Un problema', 1),
-        new Exercise('2', 'otro problema', 10)
-      ]),
-      new Homework('2', 'Otra tarea', 0, new Date(), [])
-    ];
+  constructor(private _httpService: Http) {  }
+  
+  getHomeworks():Observable<Array<Homework>> {
+    const myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');    
+    const requestOptions = new RequestOptions({headers: myHeaders});
+          
+    return this._httpService.get(this.WEB_API_URL, requestOptions)
+        .pipe(
+            map((response : Response) => <Array<Homework>> response.json()),
+            tap(data => console.log('Los datos que obtuvimos fueron: ' + JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+  }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return throwError(error.json().error|| 'Server error');
   }
 }
